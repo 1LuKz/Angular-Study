484ae11e30ed9f5b8cbc5123944305d48c61dee4
1LuKz <lucasedu292003@gmail.com>
2025-12-30 09:21:32 -0300

    Item certificate created

---
+++ certificate-generator/src/app/_components/item-certificate/item-certificate.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-certificate',
+  imports: [],
+  templateUrl: './item-certificate.html',
+  styleUrl: './item-certificate.css',
+})
+export class ItemCertificate {
+
+}

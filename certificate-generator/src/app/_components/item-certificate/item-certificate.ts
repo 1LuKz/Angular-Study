a6daa756e84fc25be1f4ac5f1645e82dca31a11d
1LuKz <lucasedu292003@gmail.com>
2025-12-30 10:02:10 -0300

    Item certificate and added container on app

--- certificate-generator/src/app/_components/item-certificate/item-certificate.ts
+++ certificate-generator/src/app/_components/item-certificate/item-certificate.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SecondaryButton } from "../secondary-button/secondary-button";
 
 @Component({
   selector: 'app-item-certificate',
-  imports: [],
+  imports: [SecondaryButton],
   templateUrl: './item-certificate.html',
   styleUrl: './item-certificate.css',
 })

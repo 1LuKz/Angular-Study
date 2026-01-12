80425d373f737b3e74ddc669b3874b3141425e61
1LuKz <lucasedu292003@gmail.com>
2026-01-12 10:35:25 -0300

    Routes created

--- certificate-generator/src/app/app.routes.ts
+++ certificate-generator/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Certifcates } from './pages/certifcates/certifcates';
+import { CertificateForm } from './pages/certificate-form/certificate-form';
+import { Certificate } from './pages/certificate/certificate';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Certifcates
+  },
+  {
+    path: "certificates/new",
+    component: CertificateForm
+  },
+  {
+    path: "certificates/:id",
+    component: Certificate
+  }
+];

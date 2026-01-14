1c114f1e6e61f477e2b9ca5ed17ff29272410a6d
1LuKz <lucasedu292003@gmail.com>
2026-01-14 16:41:06 -0300

    Using NgModel

--- certificate-generator/src/app/pages/certificate-form/certificate-form.ts
+++ certificate-generator/src/app/pages/certificate-form/certificate-form.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule],
   templateUrl: './certificate-form.html',
   styleUrl: './certificate-form.css',
 })
 export class CertificateForm {
-
+  name: string = '';
+  task: string = '';
+  tasks: string[] = [];
 }

77afe25674f95c357b1d02278a27541a10847698
1LuKz <lucasedu292003@gmail.com>
2026-01-17 12:38:23 -0300

    Certificate list

--- certificate-generator/src/app/pages/certifcates/certifcates.ts
+++ certificate-generator/src/app/pages/certifcates/certifcates.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { ItemCertificate } from "../../_components/item-certificate/item-certificate";
 import { RouterLink } from '@angular/router';
+import { certificate } from '../../interfaces/certificate';
+import { CertificateService } from '../../_services/certificate.service';
 
 @Component({
   selector: 'app-certifcates',
@@ -9,6 +11,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './certifcates.html',
   styleUrl: './certifcates.css',
 })
-export class Certifcates {
+export class Certifcates implements OnInit{
+  certificates: certificate[] = [];
 
+  constructor(private certificateService: CertificateService){
+  }
+
+  ngOnInit(): void {
+    this.certificates = this.certificateService.certificates;
+    console.log(this.certificates);
+  }
 }
